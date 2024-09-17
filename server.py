8c3e5e0111d7e5bead3826da13b53af54c73a0c2
tnfhrnsss <tnfhrns@gmail.com>
2024-09-17 03:56:39 -0400

    adapt pylink

--- server.py
+++ server.py
@@ -1,23 +1,31 @@
+"""This module emotion detector"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Analyzer")
 
+"""This route for emotion detector"""
 @app.route("/emotionDetector")
 def emotion_analyzer():
+    """This method for emotion detector"""
+
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
     dominant_emotion = response['dominant_emotion']
-    if dominant_emotion == None:
+    if dominant_emotion is None:
         return "Invalid text! Please try again!."
 
-    return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}".format(
-        response['anger'], response['disgust'], response['fear'],
-        response['joy'], response['sadness'], response['dominant_emotion'])
+    return "For the given statement, the system response is "\
+        f" 'anger': {response['anger']},"\
+        f" 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']}" \
+        f" and 'sadness': {response['sadness']}."\
+        f"The dominant emotion is {response['dominant_emotion']}"
+
 
 @app.route("/")
 def render_index_page():
+    """This method for default page"""
     return render_template('index.html')
 
 if __name__ == "__main__":
